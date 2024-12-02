6028adcd97585278fb3b7f855f7f181315a1762d
letruxux <mrgianfranco483@gmail.com>
2024-12-02 18:26:08 +0100

    get plugins of user id

--- src/routes/plugins.ts
+++ src/routes/plugins.ts
@@ -94,3 +94,10 @@ pluginsRoutes.get("/get/:name", async (c) => {
 
   return jsonResponse.success(c, repo);
 });
+
+pluginsRoutes.get("/user/:id", async (c) => {
+  const id = z.string().parse(c.req.param("id"));
+  const repos = await getPlugins(1, 100, id);
+
+  return jsonResponse.success(c, repos);
+});
